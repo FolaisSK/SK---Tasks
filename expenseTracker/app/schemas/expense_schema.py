2c844769717a5cbdd786130eb1729e8f78ed13c2
Fola <folajimibabslawal@gmail.com>
2026-04-14 14:18:22 +0100

    qed

--- expenseTracker/app/schemas/expense_schema.py
+++ expenseTracker/app/schemas/expense_schema.py
@@ -5,7 +5,7 @@ class CreateExpenseSchema(Schema):
     amount = fields.Float(required=True)
     category = fields.String(required=True)
     description = fields.String(required=True)
-    transaction_date = fields.DateTime(required=True)
+    transaction_date = fields.Date(required=False)
     user_id = fields.Integer(required=True)
 
 class UpdateExpenseSchema(Schema):
